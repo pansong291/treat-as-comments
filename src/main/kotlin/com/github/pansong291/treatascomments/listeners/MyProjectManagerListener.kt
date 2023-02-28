f1df0618faeabefae502aa42002caa358e5f4809
GitHub Action <action@github.com>
2023-02-28 03:32:03 +0000

    Template cleanup

--- src/main/kotlin/com/github/pansong291/treatascomments/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/pansong291/treatascomments/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.pansong291.treatascomments.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.pansong291.treatascomments.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/pansong291/treatascomments/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/pansong291/treatascomments/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.pansong291.treatascomments.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.pansong291.treatascomments.MyBundle
 
 class MyApplicationService {
 
